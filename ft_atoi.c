671dac48076cd39c12534d4d415fafaaa7ea1b0b
vde-frei <vde-frei@student.42sp.org.br>
2023-07-19 15:08:17 -0300

    need to done implementation of atoi  Changes to be committed: 	modified:   ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,9 +6,27 @@
 /*   By: vde-frei <vde-frei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 14:18:58 by vde-frei          #+#    #+#             */
-/*   Updated: 2023/07/19 14:32:15 by vde-frei         ###   ########.fr       */
+/*   Updated: 2023/07/19 15:08:12 by vde-frei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (TRUE);
+	return (FALSE);
+}
+
+int	ft_atoi(const char *nptr)
+{
+	while (*nptr)
+	{
+		if ((*nptr >= 9 && *nptr <= 13) || *nptr == 32)
+			nptr++;
+		if (ft_isdigit(nptr) == FALSE)
+			return (0);
+		
+	}
+}
