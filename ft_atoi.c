8e7f38473a2b7b248f65ffa14190e98abd6ab7be
vde-frei <vde-frei@student.42sp.org.br>
2023-07-19 21:11:22 -0300

    fixed atoi
     Changes to be committed:
    	modified:   ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: vde-frei <vde-frei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 14:18:58 by vde-frei          #+#    #+#             */
-/*   Updated: 2023/07/19 15:08:12 by vde-frei         ###   ########.fr       */
+/*   Updated: 2023/07/19 21:11:19 by vde-frei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 static int	ft_isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
-		return (TRUE);
-	return (FALSE);
+		return (1);
+	return (0);
 }
 
 int	ft_atoi(const char *nptr)
@@ -25,7 +25,7 @@ int	ft_atoi(const char *nptr)
 	{
 		if ((*nptr >= 9 && *nptr <= 13) || *nptr == 32)
 			nptr++;
-		if (ft_isdigit(nptr) == FALSE)
+		if (ft_isdigit(nptr) == 0)
 			return (0);
 		
 	}
