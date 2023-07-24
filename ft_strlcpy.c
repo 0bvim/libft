59cee4538b53d4cadae8ef0bd63ac980cf9c69e2
vde-frei <vde-frei@student.42sp.org.br>
2023-07-24 12:06:48 -0300

    removed main from body of code
     Changes to be committed:
    	modified:   ft_strlcpy.c

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: vde-frei <vde-frei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 11:07:26 by vde-frei          #+#    #+#             */
-/*   Updated: 2023/07/24 12:02:22 by vde-frei         ###   ########.fr       */
+/*   Updated: 2023/07/24 12:06:13 by vde-frei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,4 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	}
 	*(dst + i) = '\0';
 	return (ft_strlen(src));
-}
-
-int main(void)
-{
-	char	oi[10] = "hello";
-	char	tchau[10] = "";
-	ft_strlcpy(tchau, oi, 6);
 }
