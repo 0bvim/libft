8eb5dbd69931b37d77f63dfeb44e83edaebfeee0
Vinicius Pereira <niviciusdev@gmail.com>
2023-07-29 03:48:23 -0300

    fix: improved strnstr, but need to finish fix  Changes to be committed: 	modified:   Makefile 	modified:   ft_strnstr.c 	modified:   libft.h

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,20 +6,35 @@
 /*   By: vde-frei <vde-frei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 21:02:23 by vde-frei          #+#    #+#             */
-/*   Updated: 2023/07/29 01:11:30 by vde-frei         ###   ########.fr       */
+/*   Updated: 2023/07/29 03:47:23 by vde-frei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*strnstr(const char *big, const char *little, size_t len)
+char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	char	*string;
 	char	*tofind;
-	size_t	index;
+	size_t	tf_len;
 
 	string = (char *)big;
 	tofind = (char *)little;
+	tf_len = ft_strlen(tofind);
 	if (!tofind)
-		return (*string);
+		return (string);
+	while (len-- && *tofind != '\0')
+	{
+		if (*tofind == *string)
+		{
+			tofind++;
+			string++;
+		}
+		else
+			string++;
+	}
+	if (*tofind == '\0' && (*string || *string == '\0'))
+		return (string - tf_len);
+	else
+		return (NULL);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: vde-frei <vde-frei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/18 16:27:56 by vde-frei          #+#    #+#             */
-/*   Updated: 2023/07/28 11:58:39 by vde-frei         ###   ########.fr       */
+/*   Updated: 2023/07/29 03:08:22 by vde-frei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strchr(const char *str, int c);
 char	*ft_strrchr(const char *str, int c);
 char	*ft_strdup(const char *s);
+char	*ft_strnstr(const char *big, const char *little, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
