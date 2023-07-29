349e6112f4b2b9f799e98d1430f8cef6b0865d6d
Vinicius Pereira <niviciusdev@gmail.com>
2023-07-29 16:21:33 -0300

    fix: update function. All test 'OK'  Changes to be committed: 	modified:   ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: vde-frei <vde-frei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 21:02:23 by vde-frei          #+#    #+#             */
-/*   Updated: 2023/07/29 03:47:23 by vde-frei         ###   ########.fr       */
+/*   Updated: 2023/07/29 16:21:19 by vde-frei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,23 @@
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
-	char	*string;
-	char	*tofind;
-	size_t	tf_len;
+	char	*ocurrence;
+	size_t	lil_len;
+	size_t	c;
 
-	string = (char *)big;
-	tofind = (char *)little;
-	tf_len = ft_strlen(tofind);
-	if (!tofind)
-		return (string);
-	while (len-- && *tofind != '\0')
+	ocurrence = NULL;
+	lil_len = ft_strlen(little);
+	c = 0;
+	while (big[c] && c < len && lil_len)
 	{
-		if (*tofind == *string)
+		if (!ft_strncmp((big + c), little, lil_len) && (c + lil_len <= len))
 		{
-			tofind++;
-			string++;
+			ocurrence = (char *)(big + c);
+			break ;
 		}
-		else
-			string++;
+		c++;
 	}
-	if (*tofind == '\0' && (*string || *string == '\0'))
-		return (string - tf_len);
-	else
-		return (NULL);
+	if (!lil_len)
+		ocurrence = (char *)big;
+	return (ocurrence);
 }
